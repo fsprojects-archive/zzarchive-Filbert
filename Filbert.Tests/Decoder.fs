6a8de4a18022e993514205e17671ee964b0e1600
Yan Cui <theburningmonk@gmail.com>
2012-08-09 01:25:37 +0100

    first draft for the decoder
    - updated gitignore
    - first draft of the decoder implementation
    - some unit tests for the decoder

---
+++ Filbert.Tests/Decoder.fs
@@ -0,0 +1,109 @@
+﻿module Filbert.Tests.Decoder
+
+open System
+open System.IO
+open NUnit.Framework
+open FsUnit
+open Filbert.Decoder
+
+let test tag byteVals expected =
+    let arr = Array.append [| 131uy; tag |] byteVals
+    use stream = new MemoryStream(arr)
+
+    decode stream |> should equal expected
+
+[<TestFixture>]
+type ``Given an 8-bit small integer`` () =
+    let test = test 97uy
+
+    [<Test>]
+    member this.``when it's 0 it should return 0`` () = test [| 0uy |] 0
+
+    [<Test>]
+    member this.``when it's 255 it should return 255`` () = test [| 255uy |] 255
+
+[<TestFixture>]
+type ``Given a 32-bit integer`` () =
+    let test = test 98uy
+
+    [<Test>]
+    member this.``when it's 256 it should return 256`` () = 
+        test [| 0uy; 0uy; 1uy; 0uy |] 256
+
+    [<Test>]
+    member this.``when it's -1 it should return -1`` () = 
+        test [| 255uy; 255uy; 255uy; 255uy |] -1
+
+    [<Test>]
+    member this.``when it's 2147483647 it should return 2147483647`` () =
+        test [| 127uy; 255uy; 255uy; 255uy |] 2147483647
+
+    [<Test>]
+    member this.``when it's -2147483648 it should return -2147483648`` () =
+        test [| 128uy; 0uy; 0uy; 0uy |] -2147483648
+
+[<TestFixture>]
+type ``Given an atom`` () =
+    let test = test 100uy
+
+    [<Test>]
+    member this.``when the atom is a it should return the string 'a'`` () =
+        test [| 0uy; 1uy; 97uy |] "a"
+
+    [<Test>]
+    member this.``when the atom is abc it should return the string 'abc'`` () =
+        test [| 0uy; 3uy; 97uy; 98uy; 99uy |] "abc"
+
+    /// Note: max allowed length for atom is 255, term_to_binary(a repeated 256 times) will except
+    [<Test>]
+    member this.``when the atom is a repeated 255 times it should return a string where 'a' is repeated 255 times`` () =
+        let byteVals = Array.create<byte> 255 97uy |> Array.append [| 0uy; 255uy |] 
+        let strVal = Array.create<char> 255 'a' |> (fun arr -> new string(arr))
+        test byteVals strVal
+
+    [<Test>]
+    [<ExpectedException(typeof<InvalidAtomLength>)>]
+    member this.``when received an atom with length greater than 255 then it should except`` () =
+        let byteVals = [| 1..256 |] 
+                       |> Array.map (fun _ -> 97uy)
+                       |> Array.append [| 1uy; 0uy |] 
+        test byteVals ""
+
+    [<Test>]
+    [<ExpectedException(typeof<InvalidAtomLength>)>]
+    member this.``when received an atom with 0 length then it should except`` () =
+        test [| 0uy; 0uy |] ""
+            
+[<TestFixture>]
+type ``Given a small tuple`` () =
+    let test : byte[] -> obj[] -> unit = test 104uy
+
+    [<Test>]
+    member this.``when the tuple is empty it should return an empty array`` () =
+        test [| 0uy |] [||]
+
+    [<Test>]
+    member this.``when the tuple is { 1 } it should return an [| 1 |]`` () =
+        test [| 1uy; 97uy; 1uy |] [| 1 |]
+
+    [<Test>]
+    member this.``when the tuple is { 1, 1234, a } it should return an [| 1; "a" |]`` () =
+        test [| 3uy; 97uy; 1uy; 98uy; 0uy; 0uy; 4uy; 210uy; 100uy; 0uy; 1uy; 97uy |] [| 1; 1234; "a" |]
+
+    [<Test>]
+    member this.``when the tuple is 1 repeated 255 times ({ 1, 1, ... }) it should return [| 1; 1; ... |]`` () =
+        let byteVals = [| for i = 1 to 255 do yield! [| 97uy; 1uy |] |]
+                       |> Array.append [| 255uy |]
+        let expected = Array.create<obj> 255 1
+        test byteVals expected
+
+[<TestFixture>]
+type ``Given a large tuple`` () =
+    let test : byte[] -> obj[] -> unit = test 105uy
+
+    [<Test>]
+    member this.``when the tuple is 1 repeated 256 times ({ 1, 1, ... }) it should return [| 1; 1; ... |]`` () =
+        let byteVals = [| for i = 1 to 256 do yield! [| 97uy; 1uy |] |]
+                       |> Array.append [| 0uy; 0uy; 1uy; 0uy |]
+        let expected = Array.create<obj> 256 1
+        test byteVals expected
---
+++ Filbert/Decoder.fs
@@ -0,0 +1,100 @@
+﻿module Filbert.Decoder
+
+open System
+open System.IO
+
+// InsufficientNumberOfBytes args : required number of bytes, number of bytes read
+exception InsufficientNumberOfBytes of int * int
+exception EndOfStreamReached
+exception UnsupportTag              of int
+exception InvalidVersion            of int
+exception InvalidAtomLength         of int
+
+/// Converts a byte array with 4 elements into an int
+let bigEndianInteger arr =
+    if BitConverter.IsLittleEndian 
+    then BitConverter.ToInt32(arr |> Array.rev, 0)
+    else BitConverter.ToInt32(arr, 0)
+
+/// Converts a byte array with 2 elements into a short
+let rec bigEndianShort arr =
+    if BitConverter.IsLittleEndian
+    then BitConverter.ToInt16(arr |> Array.rev, 0)
+    else BitConverter.ToInt16(arr, 0)
+
+/// Converts a byte array into a string
+let str = System.Text.Encoding.ASCII.GetString
+
+let bigint (b : byte) = bigint (int b)
+
+/// Converts a byte array into a big integer
+let bigInteger (arr : byte[]) = 
+    arr |> Array.mapi (fun i digit -> (bigint digit) * (256I ** i)) |> Array.sum
+
+/// Reads a number of bytes from the stream
+let readBytes n (stream : Stream) = 
+    let buffer = Array.zeroCreate<byte> n
+    match stream.Read(buffer, 0, n) with
+    | 0 -> raise EndOfStreamReached
+    | n' when n' < n -> raise <| InsufficientNumberOfBytes(n, n')
+    | _ -> buffer
+
+/// Reads one byte from the stream
+let readByte (stream : Stream) = (readBytes 1 stream).[0]
+
+/// Reads a big integer from n bytes from the stream
+let readBigInt n (stream : Stream) =
+    let sign = stream |> readByte |> function | 0uy -> 1I | 1uy -> -1I
+    stream |> readBytes n |> bigInteger |> (fun n -> n * sign)
+
+/// Parses the Erlang External Term Format 
+/// see http://erlang.org/doc/apps/erts/erl_ext_dist.html
+let rec decodeType (stream : Stream) : obj =
+    match readByte stream |> int with
+    // SMALL_INTEGER (unsigned 8 bit integer) 
+    | 97  -> stream |> readByte |> int :> obj
+    // INTEGER (32 bit integer in big-endian format)
+    | 98  -> stream |> readBytes 4 |> bigEndianInteger :> obj
+    // FLOAT 
+    | 99  -> // TODO
+             raise <| System.NotImplementedException()
+    // ATOM
+    | 100 -> let len = stream |> readBytes 2 |> bigEndianShort |> int
+             match len with
+             | n when n > 255 -> raise <| InvalidAtomLength len
+             | 0 -> raise <| InvalidAtomLength len
+             | n -> stream |> readBytes n |> str :> obj
+    // SMALL_TUPLE
+    | 104 -> let arity = stream |> readByte |> int
+             match arity with
+             | 0 -> [||] :> obj
+             | n -> stream |> readTuple n :> obj
+    // LARGE_TUPLE
+    | 105 -> let arity = stream |> readBytes 4 |> bigEndianInteger
+             stream |> readTuple arity :> obj
+    // NIL
+    | 106 -> None :> obj
+    // STRING
+    | 107 -> let len = stream |> readBytes 2 |> bigEndianShort |> int
+             stream |> readBytes len |> str :> obj
+    // LIST
+    | 108 -> let len = stream |> readBytes 4 |> bigEndianInteger
+             // TODO
+             raise <| System.NotImplementedException()
+    // BINARY
+    | 109 -> let len = stream |> readBytes 4 |> bigEndianInteger
+             stream |> readBytes len :> obj
+    // SMALL_BIG
+    | 110 -> let n = stream |> readByte |> int
+             stream |> readBigInt n :> obj
+    // LARGE_BIG
+    | 111 -> let n = stream |> readBytes 4 |> bigEndianInteger
+             stream |> readBigInt n :> obj
+    | n -> raise <| UnsupportTag n
+and readTuple arity (stream : Stream) =
+    [| 1..arity |] |> Array.map (fun _ -> decodeType stream)
+
+let decode (stream : Stream) =
+    match stream |> readByte |> int with
+    | 131 -> decodeType stream
+    | n   -> raise <| InvalidVersion n
